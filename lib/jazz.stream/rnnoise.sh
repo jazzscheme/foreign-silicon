64a3d698eba18cc4ed6d0ac6c67cc6f44de30599
Guillaume Cartier <gucartier@gmail.com>
2023-09-18 10:25:37 -0400

    Update rnnoise script

--- lib/jazz.stream/rnnoise.sh
+++ lib/jazz.stream/rnnoise.sh
@@ -2,14 +2,13 @@
 
 cd ../../foreign/jazz-stream-silicon
 
-RNNOISE=/Users/barbara/Devel/gstreamer/rnnoise
+RNNOISE=/Users/cartier/Devel/gstreamer/rnnoise-silicon
 
 
-cp $RNNOISE/rnnoise/mac/lib/librnnoise.0.dylib gstreamer/lib
+cp $RNNOISE/rnnoise/silicon/lib/librnnoise.0.dylib gstreamer/lib
 cp $RNNOISE/build/plugin/libgstrnnoise.dylib gstreamer/lib/gstreamer-1.0/libgstrnnoise.dylib
 
 install_name_tool -id @rpath/librnnoise.0.dylib gstreamer/lib/librnnoise.0.dylib
-install_name_tool -change /usr/local/lib/librnnoise.0.dylib @rpath/librnnoise.0.dylib gstreamer/lib/librnnoise.0.dylib
-install_name_tool -change @rpath/librnnoise.0.dylib @rpath/gstreamer/lib/librnnoise.0.dylib gstreamer/lib/gstreamer-1.0/libgstrnnoise.dylib
+install_name_tool -change @rpath/librnnoise.dylib @rpath/librnnoise.0.dylib gstreamer/lib/gstreamer-1.0/libgstrnnoise.dylib
 
-jazz -run relocate file gstreamer/lib/gstreamer-1.0 libgstrnnoise.dylib /Library/Frameworks/GStreamer.framework/Versions/1.0 @rpath/gstreamer
+jazz -run relocate file gstreamer/lib/gstreamer-1.0 libgstrnnoise.dylib @rpath/ @rpath/lib/
